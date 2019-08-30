4dcc8b407d86575e9aca23f6760a062c50dee9ee
Richard Durazo <richarddurazo@snow.actv8server.com>
2019-08-30 10:19:34 -0700

    update config

--- server.js
+++ server.js
@@ -8,41 +8,38 @@ const path = require('path')
 // connect to database
 connectDB();
 
-//Init Middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
+app.use(multer().single('resume'));
 
-app.use(multer().single('resume'))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-
     if (req.method == "OPTIONS") {
-
         res.writeHead(200);
         res.end();
-
     } else {
-        
         next();
-
     }
-
 })
 
 // Define Routes
-
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/jobs', require('./routes/api/jobs'));
 app.use('/api/contact', require('./routes/api/contact'));
+
 // serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+    
     app.use(express.static('client/build'));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
-}
+
+
 const PORT = process.env.PORT || 5000;
+console.log('PORT', PORT)
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
