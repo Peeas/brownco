950d601cae037fd5eb02e9355a777096f17b9f29
Richard Durazo <richarddurazo@richard.local>
2019-09-30 15:28:44 -0700

    add helmet

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const helmet = require('helmet');
 const app = express();
 const multer = require('multer')
 require('dotenv').config();
 const path = require('path');
+
 // connect to database
 connectDB();
 
@@ -12,7 +13,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.use(multer().single('file'));
-
+app.use(helmet());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -30,10 +31,10 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/jobs', require('./routes/api/jobs'));
 app.use('/api/contact', require('./routes/api/contact'));
 
-// serve static assets in production
-    
-    app.use(express.static('client/build'));
 
+// serve static assets in production
+
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
