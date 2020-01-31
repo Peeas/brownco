06c35632b6e97baef91bae0479f16437643d5ea5
Richard Durazo <richarddurazo@richard.local>
2020-01-30 17:38:10 -0800

    PHASE2-19-22: issues 19 22 complete

--- client/src/components/TenantImprovement/TenantImprovement.js
+++ client/src/components/TenantImprovement/TenantImprovement.js
@@ -1,64 +1,69 @@
-import React, {Fragment} from 'react';
-import classes from './TenantImprovement.module.css'
+import React, { Fragment } from 'react';
+import classes from './TenantImprovement.module.css';
 import ContactUs from '../ContactUs/ContactUs';
-import { Parallax, Background } from 'react-parallax';
 import MoreThanDesign from '../MoreThanDesign/MoreThanDesign';
-
+import tenantImprovementImage from '../../assets/images/tenant.jpg';
+import Hero from '../UI/Hero/Hero';
 const TenantImprovement = () => {
-    window.scrollTo(0, 0);
-    return (
-        <Fragment>
-        <div className={classes.WhatWeDoContainer}>
-
-                <div className={classes.Top}>
-                    <Parallax strength={800}>
-                        <Background className={classes.Background}>
-                        </Background>
-                        <div className={classes.HomepageHero}>
-                            <div className={classes.HeroContentTitle}>
-                            Tenant Improvement
-                            </div>
-                        </div>
-                    </Parallax>
-                </div>
-                <div className={classes.titleContainer}>
-
-                    <div>
-                    Need to remodel a commercial office space? Brownco Construction Company Inc can add a bathroom and office space. Need to reconfigure a machinery line or area? Brownco can configure and install equipment foundations and assembly lines to exacting standards. No matter the project size, Brownco has the depth and experience to develop cost effective plans and provide the leadership and
-direction to manage your projects from conception to completion. 
-                    </div>
-                </div>
-                <div className={classes.SolutionsContainer}>
-                    <div>
-                        <div className={classes.StructuralSub}>Complete Solution</div>
-                        <div className={classes.SolutionsColumns}>
-                            <div>
-                            Remodel<br />
-                            Reconfigure<br />
-                            Reconstruction<br />
-                            Office Space<br />
-                            Bathroom Addition<br />
-                            Handicap Upgrade<br />
-                            Pre-Construction Planning<br />
-                            Feasibility Studies<br />
-                            Design and Engineering<br />
-                            Schedule and Budgeting<br />
-                            Permitting including Environmental<br />
-                            Construction Management<br />    
-                            </div>
- 
-                        </div>
-                    </div>
-                </div>
-
-                <MoreThanDesign />
-                
-                <div>
-                    <ContactUs />
-                </div>
-
+  window.scrollTo(0, 0);
+  return (
+    <Fragment>
+      <div className={classes.WhatWeDoContainer}>
+        <Hero
+          bgImageAlt={'renant-improvement-bg-image-brownco'}
+          heroImage={tenantImprovementImage}
+          heroText={'Tenant Improvement'}
+        />
+        <div className={classes.titleContainer}>
+          <div>
+            Need to remodel a commercial office space? Brownco Construction
+            Company Inc can add a bathroom and office space. Need to reconfigure
+            a machinery line or area? Brownco can configure and install
+            equipment foundations and assembly lines to exacting standards. No
+            matter the project size, Brownco has the depth and experience to
+            develop cost effective plans and provide the leadership and
+            direction to manage your projects from conception to completion.
+          </div>
         </div>
+        <div className={classes.SolutionsContainer}>
+          <div>
+            <div className={classes.StructuralSub}>Complete Solution</div>
+            <div className={classes.SolutionsColumns}>
+              <div>
+                Remodel
+                <br />
+                Reconfigure
+                <br />
+                Reconstruction
+                <br />
+                Office Space
+                <br />
+                Bathroom Addition
+                <br />
+                Handicap Upgrade
+                <br />
+                Pre-Construction Planning
+                <br />
+                Feasibility Studies
+                <br />
+                Design and Engineering
+                <br />
+                Schedule and Budgeting
+                <br />
+                Permitting including Environmental
+                <br />
+                Construction Management
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+        <MoreThanDesign />
+        <div>
+          <ContactUs />
+        </div>
+      </div>
     </Fragment>
-    )
-}
+  );
+};
 export default TenantImprovement;
--- client/src/components/UI/Hero/Hero.js
+++ client/src/components/UI/Hero/Hero.js
@@ -2,14 +2,22 @@ import React from 'react';
 import classes from './Hero.module.css'
 import { Parallax } from 'react-parallax';
 import defaultImg from '../../../assets/images/grey-default.png'
+const style =  {
+    backgroundSize: "cover",
+    height: '800px',
+    marginTop: '150px'
+}
 const Hero = props => {
     const heroImage = props.heroImage ? props.heroImage : defaultImg;
     return (
         <div className={classes.Top}>
             <Parallax
+                bgImageStyle={props.aboutUs ? style : {display: 'flex'}}
                 bgImage={heroImage}
+                bgImageAlt={props.bgImageAlt}
                 strength={600}>
                     <div className={classes.ParallaxDiv}>
+                        { props.isLanding ? <div className={classes.HeroContentText}>{props.subText}</div> : '' }
                         <div className={classes.HeroContentTitle}>
                             {props.heroText}
                         </div>
