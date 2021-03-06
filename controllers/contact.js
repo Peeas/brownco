0edf86b5db05dcbf9e482e0de4ff6ddb5124a3e0
Richard Durazo <rcdurazo@gmail.com>
2021-03-05 17:16:47 -0800

    Update email

--- controllers/contact.js
+++ controllers/contact.js
@@ -19,7 +19,7 @@ exports.postContactemail = async (req, res, next) => {
     try {
         const { firstName, lastName, companyName, email, phone, service, comment } = req.body;
         const newEmail = await transporter.sendMail({
-            to: 'lindy.browncoinc@gmail.com', 
+            to: 'abrown@browncoinc.com', 
             from: email,
             subject: `brownco contact us: ${service}`,
             html: `<div>Hello Brownco,</div> <br>
